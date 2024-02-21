f4176a39d22aabe5dea9a5eed2e7c804f64fe277
Andy Z <andy@DN0a24476f.SUNet>
2024-02-20 16:41:06 -0800

    add util

--- common/util.py
+++ common/util.py
@@ -2,6 +2,7 @@ from light_tokenizer import LightTokenizer, DefaultTokenizer
 import json
 import cattrs
 from data_overlap_spec import AggregateOverlapMetric
+from light_scenario import ScenarioSpecInstanceIds
 
 def get_tokenizer(normalization) -> LightTokenizer:
     if normalization == "none":
@@ -24,6 +25,42 @@ def load_aggregate_metrics(file_path: str):
                 print(f"Error reading line: {err}")
                 continue  # Proceed to next line or handle error as needed
     return aggregate_metrics
+
+def scenario_spec_to_class(scenario_spec) -> str:
+#     return scenario_spec.class_name
+    return scenario_spec.class_name.split('.')[-1][:-8]
+
+
+def read_scenario_spec_instance_ids_json():
+    scenario_spec_instance_ids_json = 'filtered_scenario_spec_instance_ids.json'
+    scenario_spec_instance_ids_jsons = open(scenario_spec_instance_ids_json, "r").readlines()
+    return scenario_spec_instance_ids_jsons
+
+def get_scenario_spec_instance_id_dict():
+    scenario_spec_instance_ids_jsons = read_scenario_spec_instance_ids_json()
+    scenario_spec_instance_id_dict = dict()
+    for scenario_spec_instance_ids_json in scenario_spec_instance_ids_jsons:
+        scenario_spec_instance_ids_dict = json.loads(scenario_spec_instance_ids_json)
+        scenario_spec_instance_ids = cattrs.structure(scenario_spec_instance_ids_dict, ScenarioSpecInstanceIds)
+        scenario_spec_instance_id_dict[
+            scenario_spec_instance_ids.scenario_spec
+        ] = scenario_spec_instance_ids.instance_ids
+
+def get_class_name_to_counts():
+    scenario_spec_instance_ids_jsons = read_scenario_spec_instance_ids_json()
+    class_name_to_counts = dict()
+    scenario_spec_instance_id_dict = dict()
+    for scenario_spec_instance_ids_json in scenario_spec_instance_ids_jsons:
+        scenario_spec_instance_ids_dict = json.loads(scenario_spec_instance_ids_json)
+        scenario_spec_instance_ids = cattrs.structure(scenario_spec_instance_ids_dict, ScenarioSpecInstanceIds)
+        scenario_spec_instance_id_dict[
+            scenario_spec_instance_ids.scenario_spec
+        ] = scenario_spec_instance_ids.instance_ids
+        class_name = scenario_spec_to_class(scenario_spec_instance_ids.scenario_spec)
+        if class_name not in class_name_to_counts:
+            class_name_to_counts[class_name] = 0
+        class_name_to_counts[class_name] += len(scenario_spec_instance_ids.instance_ids)
+    return class_name_to_counts
 
 
 
